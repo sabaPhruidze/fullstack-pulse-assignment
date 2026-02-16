244690f83a2551587e6bcd07192f4238d6fae74a
Saba <sabaphruidze01@gmail.com>
2026-02-16 17:34:03 +0400

    update(Dashboard): used PageLayout and removed unnecesarry things

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import PortfolioSummaryCard from "./dashboard/PortfolioSummaryCard";
 import TopMoversCard from "./dashboard/TopMoversCard";
 import LatestNewsCard from "./dashboard/LatestNewsCard";
 import ActiveAlertsCard from "./dashboard/ActiveAlertsCard";
+import PageLayout from "../components/layout/PageLayout";
 const Dashboard = () => {
   const { data, isLoading, isError, error } = useDashboardData();
   const assets = data?.assets;
@@ -11,33 +12,29 @@ const Dashboard = () => {
   const news = data?.news ?? { success: true, count: 0, data: [] };
   const alerts = data?.alerts ?? { success: true, count: 0, data: [] };
   return (
-    <div className="bg-pulse-bg min-h-screen">
-      <Header />
-      <div className="p-5 ">
-        <h1 className="text-3xl font-bold mb-2 text-pulse-text">Dashboard</h1>
-        <p className="text-sm text-pulse-soft font-semibold">
-          Portfolio snapshot and market overview
-        </p>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : isError ? (
-          <div className="mt-4 text-red-400">
-            Error loading Dashboard data
-            <div className="text-xs opacity-80 mt-1">
-              {error instanceof Error ? error.message : "Unknown error"}
-            </div>
+    <PageLayout
+      title="Dashboard"
+      subtitle="Portfolio snapshot and market overview"
+    >
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : isError ? (
+        <div className="mt-4 text-red-400">
+          Error loading Dashboard data
+          <div className="text-xs opacity-80 mt-1">
+            {error instanceof Error ? error.message : "Unknown error"}
           </div>
-        ) : (
-          <>
-            <PortfolioSummaryCard summary={data?.portfolio.data} />
-            <TopMoversCard title="Top Gainers" assets={list} mode="gainers" />
-            <TopMoversCard title="Top Losers" assets={list} mode="losers" />
-            <LatestNewsCard news={news} />
-            <ActiveAlertsCard alerts={alerts} />
-          </>
-        )}
-      </div>
-    </div>
+        </div>
+      ) : (
+        <>
+          <PortfolioSummaryCard summary={data?.portfolio.data} />
+          <TopMoversCard title="Top Gainers" assets={list} mode="gainers" />
+          <TopMoversCard title="Top Losers" assets={list} mode="losers" />
+          <LatestNewsCard news={news} />
+          <ActiveAlertsCard alerts={alerts} />
+        </>
+      )}
+    </PageLayout>
   );
 };
 
