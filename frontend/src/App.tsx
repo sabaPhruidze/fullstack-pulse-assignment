cf9048394003cdd858f6247ea4d161785d002376
Saba <sabaphruidze01@gmail.com>
2026-03-18 11:04:57 +0400

    feat(app + ErrorFallBack): added Error Boundary and created fallback component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallBack from "./components/shared/ErrorFallBack";
 
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Assets = lazy(() => import("./pages/Assets"));
 const News = lazy(() => import("./pages/News"));
 const Alerts = lazy(() => import("./pages/Alerts"));
+
 function App() {
   return (
     <BrowserRouter
@@ -13,14 +16,16 @@ function App() {
         v7_relativeSplatPath: true,
       }}
     >
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/assets" element={<Assets />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/alerts" element={<Alerts />} />
-        </Routes>
-      </Suspense>
+      <ErrorBoundary FallbackComponent={ErrorFallBack}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/assets" element={<Assets />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/alerts" element={<Alerts />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/shared/ErrorFallBack.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorFallBack = () => {
+  return (
+    <div>ErrorFallBack</div>
+  )
+}
+
+export default ErrorFallBack
