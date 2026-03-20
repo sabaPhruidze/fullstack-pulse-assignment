b42c0aed89f92b6e6aed2d3a0aed48017ce781f4
Saba <sabaphruidze01@gmail.com>
2026-03-20 13:36:04 +0400

    feat(app):updated style in app folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/shared/ErrorFallBack";
 import Loading from "./components/shared/Loading";
+import useThemeStore from "./store/useThemeStore";
 
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Assets = lazy(() => import("./pages/Assets"));
@@ -10,6 +11,11 @@ const News = lazy(() => import("./pages/News"));
 const Alerts = lazy(() => import("./pages/Alerts"));
 
 function App() {
+  const theme = useThemeStore((state) => state.theme);
+  useEffect(() => {
+    //document.documentElement is <html>  and classList.toggle("dark", theme === "dark") means if it is dark add it or if it is not remove it
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
   return (
     <BrowserRouter
       future={{
@@ -17,16 +23,18 @@ function App() {
         v7_relativeSplatPath: true,
       }}
     >
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/assets" element={<Assets />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/alerts" element={<Alerts />} />
-          </Routes>
-        </Suspense>
-      </ErrorBoundary>
+      <div className="min-h-screen bg-slate-50 text-slate-900 transition-colors duration-300 dark:bg-slate-950 dark:text-slate-100">
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/assets" element={<Assets />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/alerts" element={<Alerts />} />
+            </Routes>
+          </Suspense>
+        </ErrorBoundary>
+      </div>
     </BrowserRouter>
   );
 }
