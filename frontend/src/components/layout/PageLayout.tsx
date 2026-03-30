22abf72fbdd13b89500d90d9de9bb971049ef2cb
Saba <sabaphruidze01@gmail.com>
2026-03-30 09:58:03 +0400

    style(Pagelayout): Mainly corrected h1 and p but also added min-w-0 on children in order to make them compress inside text and not to throw out them

--- frontend/src/components/layout/PageLayout.tsx
+++ frontend/src/components/layout/PageLayout.tsx
@@ -11,17 +11,21 @@ function PageLayout({ title, subtitle, children }: Props) {
     <div className="min-h-screen bg-slate-50 dark:bg-pulse-bg">
       <div className="flex min-h-screen">
         <DesktopSideBar />
-        <div className="flex-1">
+        <div className="min-w-0 flex-1">
           <Header />
-          <main className="p-4 sm:p-5 md:p-6 lg:p-8">
-            <h1 className="mb-2 text-2xl font-bold text-pulse-800 dark:text-pulse-text sm:text-3xl">
-              {title}
-            </h1>
-            <p className="mn-4 text-sm font-semibold text-slate-500 dark:text-pulse-soft">
-              {subtitle}
-            </p>
+          <main className="p-4 w-full sm:p-5 md:p-6 lg:p-8 xl:px-10">
+            <div mx-auto w-full max-w-7xl>
+              <div className="mb-5 sm:mb-6">
+                <h1 className="mb-2 text-2xl font-bold text-pulse-800 dark:text-pulse-text sm:text-3xl lg:text-4xl">
+                  {title}
+                </h1>
+                <p className="m-4 text-sm font-semibold text-slate-500 dark:text-pulse-soft ">
+                  {subtitle}
+                </p>
 
-            {children}
+                <div className="min-w-0"> {children}</div>
+              </div>
+            </div>
           </main>
         </div>
       </div>
