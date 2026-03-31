3d4cf82efbaabf3889f267cefa76010bc2e3f868
Saba <sabaphruidze01@gmail.com>
2026-03-31 10:11:32 +0400

    test(News):checked the opening of dropdown menu and if on crypto click function receives crypto

--- frontend/src/test/NewsCategoryFilter.test.tsx
+++ frontend/src/test/NewsCategoryFilter.test.tsx
@@ -6,7 +6,18 @@ import NewsCategoryFilter from "../components/ui/NewsCategoryFilter";
 describe("NewsCategoryFilter", () => {
   test("default value all is shown on button", () => {
     render(<NewsCategoryFilter value="all" onChange={vi.fn()} />);
-    screen.debug(); //this shows finally what is rendered in DOM
+    //screen.debug(); this shows finally what is rendered in DOM
     expect(screen.getByRole("button", { name: /all/i })).toBeInTheDocument();
+  });
+  test("on Choose of crypto in onChange must call using crypto", async () => {
+    const user = userEvent.setup();
+    const onChange = vi.fn();
+    render(<NewsCategoryFilter value="all" onChange={onChange} />);
+    const filterButton = screen.getByRole("button", { name: /all/i });
+    await user.click(filterButton);
+    const cryptoOption = screen.getByRole("button", { name: /crypto/i });
+    await user.click(cryptoOption);
+    expect(onChange).toBeCalledTimes(1); // onChange it will be called 1 time
+    expect(onChange).toHaveBeenCalledWith("crypto"); // check that it function received crypto
   });
 });
