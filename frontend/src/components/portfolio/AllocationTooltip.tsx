ac0d63527507c7fc8195d57cead7bdb13ec0b0ed
Saba <sabaphruidze01@gmail.com>
2026-04-02 10:03:09 +0400

    feat(allocationTooltip):made it adjusted to dark and light mode

--- frontend/src/components/portfolio/AllocationTooltip.tsx
+++ frontend/src/components/portfolio/AllocationTooltip.tsx
@@ -17,17 +17,6 @@ export const formatCurrency = (value: number) => {
     maximumFractionDigits: 2,
   }).format(value);
 };
-const getTooltipTextColor = (hexColor: string) => {
-  const hex = hexColor.replace("#", "");
-
-  const r = parseInt(hex.slice(0, 2), 16);
-  const g = parseInt(hex.slice(2, 4), 16);
-  const b = parseInt(hex.slice(4, 6), 16);
-
-  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-
-  return brightness > 160 ? "#0f172a" : "#ffffff";
-};
 
 const AllocationToolTip = ({
   active,
@@ -39,16 +28,18 @@ const AllocationToolTip = ({
   const itemPayload = item.payload as AllocationPayload;
   const numericValue =
     typeof item.value === "number" ? item.value : Number(item.value ?? 0);
-  const bgColor = item.color ?? "#0f172a";
-  const textColor = getTooltipTextColor(bgColor);
+
   return (
-    <div
-      className="rounded-xl px-3 py-2 shadow-lg"
-      style={{ backgroundColor: bgColor, color: textColor }}
-    >
-      <p className="text-sm font-semibold">{itemPayload.name}</p>
-      <p className="text-xs">Value: {formatCurrency(numericValue)}</p>
-      <p className="text-sm">Share: {item.payload.percentage.toFixed(2)}%</p>
+    <div className="rounded-xl border border-slate-400 bg-white px-3 py-2 shadow-lg dark:border-pulse-border dark:bg-pulse-surface">
+      <p className="text-sm font-semibold text-slate-800 dark:text-pulse-text">
+        {itemPayload.name}
+      </p>
+      <p className="text-xs text-slate-800 dark:text-pulse-text">
+        Value: {formatCurrency(numericValue)}
+      </p>
+      <p className="text-sm text-slate-800 dark:text-pulse-text">
+        Share: {item.payload.percentage.toFixed(2)}%
+      </p>
     </div>
   );
 };
